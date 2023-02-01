9e8dcf8c602fe59140f794487dd07e2defaf11e2
volan4ik2003 <yanc359@gmail.com>
2023-02-01 20:21:41 +0200

    Added camera slider

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
@@ -11,13 +12,15 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float zoomSpeed = 10f;
     [SerializeField] private float camOrthSizeMax = 67f, camOrthSizeMin = 10f;
     [SerializeField] private float TopCamLimit = 120f, downCamLimit = 5f, leftCamLimit = -120f, rightCamLimit = 120f;
+    [SerializeField] private Slider camHighSlider;
     float horBound;
     float vertBound;
     float leftBound, rightBound, upBound, downBound;
 
     private void Start()
     {
-        cam.orthographicSize = camOrthSizeMin + 0.1f;
+        cam.orthographicSize = camOrthSizeMin + 10f;
+        cam.transform.position = new Vector3(0, 0, -10);
         
     }
 
@@ -30,9 +33,9 @@ public class CameraController : MonoBehaviour
         upBound = transform.position.y + vertBound;
         downBound = transform.position.y - vertBound;
 
-        Debug.Log(rightBound);
-
         Vector3 pos = transform.position;
+
+        camHighSlider.value = pos.y;
 
         if (upBound < TopCamLimit)
         {
